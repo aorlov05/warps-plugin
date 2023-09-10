d8cf7cd9b83ed69e13edee10df45a4908031e84d
aorlov05 <andreworlov2005@gmail.com>
2023-09-10 19:51:31 -0400

    Initial commit

---
+++ src/main/java/io/github/aorlov05/warps/WarpsPlugin.java
@@ -0,0 +1,17 @@
+package io.github.aorlov05.warps;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class WarpsPlugin extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+
+    }
+
+    @Override
+    public void onDisable() {
+
+    }
+
+}
